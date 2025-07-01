9cbf26a3b34ce85aa73cadfe54cc5062b77d0440
andra <andrabaktiangjaya@gmail.com>
2025-07-02 00:22:55 +0800

    navbar responsive 2

--- Meeting 3/src/main.js
+++ Meeting 3/src/main.js
@@ -2,11 +2,6 @@ document.addEventListener('DOMContentLoaded', () => {
     const menuToggle = document.getElementById('menu-toggle');
     const mobileMenu = document.getElementById('mobile-menu');
 
-    if (!menuToggle || !mobileMenu) {
-        console.error('menuToggle or mobileMenu not found');
-        return;
-    }
-
     menuToggle.addEventListener('click', () => {
         mobileMenu.classList.toggle('hidden');
     });
